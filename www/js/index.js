d9d0fc9fb5d817b63f02b854af7eea3f0fd3b9a4
reik2k <reik2k@hotmail.com>
2014-10-30 17:46:12 +0100

    'T4EX1:Terminado'

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        app.checkNetwork();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,5 +46,20 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    checkNetwork:function() {
+    var networkState = navigator.connection.type;
+        var states = {};
+        
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
+        
+        alert('Connection type: ' + states[networkState]);
     }
 };
